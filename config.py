d67e9f88244804ea1338e42018b43e336f44506f
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-16 22:12:31 +0530

    V-6

---
+++ config.py
@@ -0,0 +1,16 @@
+LOG_GROUP_ID = -1001768984791
+OWNER_ID = [1985665341]
+HANDLER = ["~", ".", "!"]
+ALIVE_TEXT = """ **~  Hydra System:**
+━━━━━━━━━━━━━━━━━━━
+
+❥ **𝑂𝑊𝑁𝐸𝑅**: {}
+❥ **𝑉𝐸𝑅𝑆𝐼𝑂𝑁:** {}
+❥ **𝑃𝑂𝑁𝐺 𝑀𝑠:** {}
+❥ **𝑈𝑃𝑇𝐼𝑀𝐸:** {}
+
+━━━━━━━━━━━━━━━━━━━
+**Meet Me Here🙈 @FutureCity005 ✨🥀**
+"""
+
+Hydra = "https://telegra.ph/file/af2a44b84e649a0a60679.jpg"
