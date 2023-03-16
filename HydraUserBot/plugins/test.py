3b6d53487dea4a6e4c31cb22d852ca6c11513000
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-16 23:26:11 +0530

    V-6

---
+++ HydraUserBot/plugins/test.py
@@ -0,0 +1,9 @@
+from HydraUserBot import Hydra
+
+OWNER_ID = "1985665341"
+
+@Hydra.on_message(filters.command("kick", prefixes=HANDLER) & filters.user(OWNER_ID))
+
+await client.ban_chat_member(message.chat.id, f"{message.reply_to_message.from_user.id}")
+
+await client.unban_chat_member(message.chat.id, f"{message.reply_to_message.from_user.id}")
