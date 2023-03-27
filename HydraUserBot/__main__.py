c36ad11dc685752630372e50b63ddaad1ded96ba
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-27 21:40:34 +0530

    Update __main__.py

--- HydraUserBot/__main__.py
+++ HydraUserBot/__main__.py
@@ -3,6 +3,3 @@ from HydraUserBot import bot, Hydra
 
 if __name__ == "__main__":
     Hydra.start()
-    bot.run()
-    with bot:
-        bot.send_message(f"{LOG_GROUP_ID}", "Nyaa Hydra Ready Boi!")
