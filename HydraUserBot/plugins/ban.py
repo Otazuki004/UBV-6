079feac1e48a7f883f52a603ed6a24fd90af9f5c
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-27 21:52:49 +0530

    Update ban.py

--- HydraUserBot/plugins/ban.py
+++ HydraUserBot/plugins/ban.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 @bot.on_message(filters.command("ban") & filters.user(1985665341)) 
 def ban(bot, message):
 	reply = m.reply_to_message
-_reply = ""
+reply = ""
 if not reply:
 	bot.send_message(message.chat.id, "Please Reply Someone To Ban")
 if reply.from_user:
