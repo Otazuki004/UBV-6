36c4285c7d9ed93e2fb75457080ba183d96a6164
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-27 21:48:01 +0530

    Error fix #7

---
+++ HydraUserBot/__main__.py
@@ -0,0 +1,8 @@
+from config import LOG_GROUP_ID
+from HydraUserBot import bot, Hydra
+
+if __name__ == "__main__":
+    Hydra.start()
+    bot.run()
+    with bot:
+        bot.send_message(f"{LOG_GROUP_ID}", "UB Ready")
