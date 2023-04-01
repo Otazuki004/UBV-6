09205824aa9f85236867082257cbd674f2dd6c15
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-04-01 15:52:54 +0530

    Update Kick.py

--- HydraUserBot/plugins/Kick.py
+++ HydraUserBot/plugins/Kick.py
@@ -4,6 +4,5 @@ from pyrogram import filters
 @bot.on_message(filters.command("kick") & filters.user(1985665341)) 
 def ban(bot, message):
 	bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-     bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-     bot.send_message(message.chat.id, f"Successfully Kicked! {message.reply_to_message.from_user.mention}.")
-)
+	bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+	bot.send_message(message.chat.id, f"Successfully Kicked! {message.reply_to_message.from_user.mention}.")
