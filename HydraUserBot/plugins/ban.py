6fde71f33e92475d4aa35f7ce7d4dbfa6edd61c1
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-04-01 15:53:55 +0530

    Update ban.py

--- HydraUserBot/plugins/ban.py
+++ HydraUserBot/plugins/ban.py
@@ -2,7 +2,6 @@ from HydraUserBot import Hydra as bot
 from pyrogram import filters
 
 @bot.on_message(filters.command("ban") & filters.user(1985665341)) 
-def ban(bot, message)
+def ban(bot, message):
 	bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-bot.send_message(message.chat.id, f"Successfully Banned! {message.reply_to_message.from_user.mention}.")
-)
+	bot.send_message(message.chat.id, f"Successfully Banned! {message.reply_to_message.from_user.mention}.")
