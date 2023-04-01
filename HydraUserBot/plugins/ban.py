daf5b7849fa8104d2d927f30fdab1665191053cd
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-04-01 16:28:16 +0530

    Update ban.py

--- HydraUserBot/plugins/ban.py
+++ HydraUserBot/plugins/ban.py
@@ -7,6 +7,6 @@ def ban(bot, message):
     _reply = ""
     if not reply:
         bot.send_message(message.chat.id, "Please Reply Someone To Ban")
-   if reply.from_user:
-        bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-        bot.send_message(message.chat.id, f"Successfully Banned! {message.reply_to_message.from_user.mention}."),
+        if reply.from_user:
+            bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+            bot.send_message(message.chat.id, f"Successfully Banned! {message.reply_to_message.from_user.mention}."),
