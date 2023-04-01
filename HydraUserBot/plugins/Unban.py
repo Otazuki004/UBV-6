778ca5e084e6024c2b26db168e5ffbe784a9bfed
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-04-01 15:51:42 +0530

    Update Unban.py

--- HydraUserBot/plugins/Unban.py
+++ HydraUserBot/plugins/Unban.py
@@ -4,5 +4,4 @@ from pyrogram import filters
 @bot.on_message(filters.command("kick") & filters.user(1985665341)) 
 def ban(bot, message):
 	bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-     bot.send_message(message.chat.id, f"Successfully Unbaned! {message.reply_to_message.from_user.mention}.")
-)
+	bot.send_message(message.chat.id, f"Successfully Unbaned! {message.reply_to_message.from_user.mention}.")
