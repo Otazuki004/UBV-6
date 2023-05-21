4509c83f70a847f4d55dea98c490a5c5e92d20af
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-21 18:55:21 +0530

    Downgrade Kick.py

--- HydraUserBot/plugins/Kick.py
+++ HydraUserBot/plugins/Kick.py
@@ -7,9 +7,6 @@ def ban(bot, message):
 	bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
 	bot.send_message(message.chat.id, f"Successfully Kicked! {message.reply_to_message.from_user.mention}.")
 	
-@bot.on_message(filters.command("ban") & filters.user(1985665341)) 
-def ban(bot, message):
-	
 	
 
 	
