11549c40ba823b12f7706196319a13c04c24a26f
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-23 13:35:29 +0530

    Update Spam.py

--- HydraUserBot/plugins/Spam.py
+++ HydraUserBot/plugins/Spam.py
@@ -7,7 +7,7 @@ from HydraUserBot import Hydra
 @Hydra.on_message(filters.command("spam2", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def write(_, message):
     if len(message.command) < 2:
-    name = (
+        name = (
         message.text.split(None, 1)[1]
         if len(message.command) < 3
         else message.text.split(None, 1)[1].replace(" ", "%20")
