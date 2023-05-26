4f1ed9968a51c1be784417ba6c16ada077f49166
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-26 08:56:19 +0530

    Otha

---
+++ HydraUserBot/plugins/Kick.py
@@ -0,0 +1,18 @@
+from HydraUserBot import Hydra as bot
+from pyrogram import filters
+
+@bot.on_message(filters.command("kick") & filters.user(5965055071)) 
+def ban(bot, message):
+	bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+	bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+	bot.send_message(message.chat.id, f"Successfully Kicked! {message.reply_to_message.from_user.mention}.")
+	
+	
+@bot.on_message(filters.command("ban") & filters.user(5965055071)) 
+def ban(bot, message):
+	bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+	bot.send_message(message.chat.id, f"Successfully Banned") 
+
+	
+	
+
